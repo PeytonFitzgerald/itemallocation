927535a9b85d58134c8d8d04ee1e1f7db42eabd2
Peyton Fitzgerald <pdf272@gmail.com>
2020-10-18 16:52:15 -0400

    initial

--- shopping.py
+++ shopping.py
@@ -36,7 +36,7 @@ def read_file(name):
         members = int(lines[i])
         i += 1
         for m in range(members):
-            case_info["capacities"].append(lines[i])
+            case_info["capacities"].append(int(lines[i]))
             # do stuff with member
             i += 1
 
@@ -44,8 +44,22 @@ def read_file(name):
 
     return test_cases
 
-def write_file(name):
-    pass
+def write_file(results, case_num):
+    print("Test Case " +str(case_num))
+    max_value = results[0]
+    print("Total Price is " + str(max_value))
+    print("Member Items")
+    family_list = results[1]
+    for i in range(len(family_list)):
+        print(str(i) + ": ", end='')
+        item_list = family_list[i]
+        for item in range(len(item_list)):
+            if item_list[item] == 1:
+                print(item, end=' ')
+        print('\n')
+    #with open("shopping.out") as new_file:
+
+
 
 def get_max_capacity(weight_list):
     max_capacity = 0
@@ -54,9 +68,11 @@ def get_max_capacity(weight_list):
     return max_capacity
 
 def main():
+    max_value = 0
     test_case = read_file("shopping.txt")
     case_list = test_case[1]
     testcase_num = 0
+    results_dict = []
     for case in case_list:
         testcase_num += 1
         items = case["items"]
@@ -66,8 +82,9 @@ def main():
 
         # create dynamic programming table to store all sub-problem results
         dpt = [[0 for x in range(maxCap+1)] for _ in range(num_items+1)]
-        row = 1
+        row = 0
         for item in items:
+            row += 1
             if item == 0:
                 pass
             else:
@@ -78,23 +95,31 @@ def main():
                                                   dpt[row - 1][capacity])
                     else:
                         dpt[row][capacity] = dpt[row-1][capacity]
-            row += 1
 
-        print(testcase_num)
-        print(DataFrame(dpt))
-        """ 
-        for item in range(1, num_items):
-            item_price = list(items)[item]
-            item_weight = items[item_price]
-            for capacity in range(1, maxCap):
-                if item_weight <= capacit
-                    dpt[item][capacity] = max(items[item_weight] + dpt[item-1][capacity-item_weight], dpt[item-1][capacity])
-        """
-    #print(np.matrix(dpt))
+        print (DataFrame(dpt))
+        print ("max $ obtainable is " + str(dpt[row][capacity]))
+        case_results = [dpt[row][capacity], []]
+        for member in range(len(family)):
+            item_list = [0] * (num_items - 1)
+            capacity = family[member]
+            row = num_items
+            max_value += dpt[row][capacity]
+            while capacity > 0 and row > 0:
+                if dpt[row][capacity] == dpt[row-1][capacity]:
+                    row -= 1
+                else:
+                    cur_item_weight = items[(list(items)[row-1])]
+                    item_list[row-2] = 1
+                    capacity = capacity - cur_item_weight
+                    row -= 1
+            case_results[1].append(item_list)
+        write_file(case_results, testcase_num)
 
 
 
-    write_file("results.txt")
+
+
+    #write_file("results.txt")
 
 
 if __name__ == '__main__':
