f5fefcbb351715e71e15b9bf4827f913547c9a48
Peyton Fitzgerald <pdf272@gmail.com>
2020-10-19 00:06:35 -0400

    near finished

--- shopping.py
+++ shopping.py
@@ -44,20 +44,41 @@ def read_file(name):
 
     return test_cases
 
-def write_file(results, case_num):
-    print("Test Case " +str(case_num))
+def write_file(results_list):
+    """
+    print("Test Case " + str(case_num))
     max_value = results[0]
     print("Total Price is " + str(max_value))
     print("Member Items")
     family_list = results[1]
-    for i in range(len(family_list)):
-        print(str(i) + ": ", end='')
-        item_list = family_list[i]
+    num = 0
+    for i in range(len(family_list)-1, -1, -1):
+        num += 1
+        print(str(num) + ": ", end='')
+        item_list = family_list[i-1]
         for item in range(len(item_list)):
             if item_list[item] == 1:
-                print(item, end=' ')
-        print('\n')
-    #with open("shopping.out") as new_file:
+                print(item+1, end=' ')
+        print()
+    """
+
+    with open("shopping.out", 'w') as f:
+        case_num = 0
+        for results in results_list:
+            case_num += 1
+            max_value = results[0]
+            f.write('Test Case ' + str(case_num) +'\n' + 'The total price is ' + str(max_value) + '\n' + 'Member Items\n')
+            family_list = results[1]
+            num = 0
+            for i in range(len(family_list) - 1, -1, -1):
+                num += 1
+                f.write(str(num) + ": ")
+                item_list = family_list[i - 1]
+                for item in range(len(item_list)):
+                    if item_list[item] == 1:
+                        f.write(str(item+1) + ' ')
+                f.write('\n')
+            f.write('\n')
 
 
 
@@ -68,12 +89,12 @@ def get_max_capacity(weight_list):
     return max_capacity
 
 def main():
-    max_value = 0
+    results_list = []
     test_case = read_file("shopping.txt")
     case_list = test_case[1]
     testcase_num = 0
-    results_dict = []
     for case in case_list:
+        max_value = 0
         testcase_num += 1
         items = case["items"]
         family = case["capacities"]
@@ -83,23 +104,25 @@ def main():
         # create dynamic programming table to store all sub-problem results
         dpt = [[0 for x in range(maxCap+1)] for _ in range(num_items+1)]
         row = 0
-        for item in items:
+        for item in items: # move through DP table one row (item) at a time
             row += 1
-            if item == 0:
+            if item == 0: # skip first row
                 pass
             else:
                 item_weight = items[item]
                 for capacity in range(1, maxCap+1):
+                    # if item can fit into carrying capacity
                     if item_weight <= capacity:
+                        # check if it's worth it to take this item + any remainder, or to just take from above
                         dpt[row][capacity] = max(item + dpt[row - 1][capacity - item_weight],
                                                   dpt[row - 1][capacity])
                     else:
+                        # if can't take just grab from above
                         dpt[row][capacity] = dpt[row-1][capacity]
 
-        print (DataFrame(dpt))
-        print ("max $ obtainable is " + str(dpt[row][capacity]))
-        case_results = [dpt[row][capacity], []]
+        case_results = [0, []]
         for member in range(len(family)):
+
             item_list = [0] * (num_items - 1)
             capacity = family[member]
             row = num_items
@@ -113,9 +136,9 @@ def main():
                     capacity = capacity - cur_item_weight
                     row -= 1
             case_results[1].append(item_list)
-        write_file(case_results, testcase_num)
-
-
+        case_results[0] = max_value
+        results_list.append(case_results)
+    write_file(results_list)
 
 
 
